362b8d8a5b9012446570bcfc988f8718d43e4d84
Philipp Haupt <philipp.haupt@swisscom.com>
2018-07-12 17:04:06 +0200

    Updated

--- src/com/swisscom/atclient/ATresponder.java
+++ src/com/swisscom/atclient/ATresponder.java
@@ -141,32 +141,13 @@ public class ATresponder extends Thread {
 		// 3G = UTRAN is Universal Terrestial Radio Access Network 
 		// 4G = E-UTRAN (Evolved Universal Terrestial Radio Access Network)
 		
-		// # Registration States (SECOND number of AT+CREG?) from http://m2msupport.net/m2msupport/atcreg-network-registration/
-		//		AT+CREG AT command gives information about the registration status and access technology of the serving cell.
-		//
-		//		Possible values of registration status are,
-		//		 0 not registered, MT is not currently searching a new operator to register to
-		//		 1 registered, home network
-		//		 2 not registered, but MT is currently searching a new operator to register to
-		//		 3 registration denied
-		//		 4 unknown (e.g. out of GERAN/UTRAN/E-UTRAN coverage)
-		//		 5 registered, roaming
-		//		 6 registered for "SMS only", home network (applicable only when indicates E-UTRAN)
-		//		 7 registered for "SMS only", roaming (applicable only when indicates E-UTRAN)
-		//		 8 attached for emergency bearer services only (see NOTE 2) (not applicable)
-		//		 9 registered for "CSFB not preferred", home network (applicable only when indicates E-UTRAN)
-		//		 10 registered for "CSFB not preferred", roaming (applicable only when indicates E-UTRAN)
-		//
-		//		Possible values for access technology are,
-		//		 0 GSM
-		//		 1 GSM Compact
-		//		 2 UTRAN
-		//		 3 GSM w/EGPRS
-		//		 4 UTRAN w/HSDPA
-		//		 5 UTRAN w/HSUPA
-		//		 6 UTRAN w/HSDPA and HSUPA
-		//		 7 E-UTRAN
-		send("AT+CREG=1", "AT+CREG=1", false); // Enable CREG
+		//		0  nicht eingebucht, keine Netzsuche 
+		//		1 eingebucht, Heimatnetz 
+		//		2 nicht eingebucht, Netzsuche 
+		//		3 nicht eingebucht, Einbuchung abgelehnt 
+		//		4 Status unbekannt 
+		//		5 eingebucht, Fremdnetz 
+		send("AT+CREG=2", "AT+CREG=2", false); // Enable CREG
 		send("AT+CREG?", "+CREG", false); // REGISTRATION_STATE
 		
 		send("AT+CIMI", "AT+CIMI", false); // IMSI
