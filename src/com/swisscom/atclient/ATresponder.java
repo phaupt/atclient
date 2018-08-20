d5e324f495f45cd3166df952e91e953ebd9a4a40
Philipp Haupt <philipp.haupt@swisscom.com>
2018-08-20 09:12:25 +0200

    Updated

--- src/com/swisscom/atclient/ATresponder.java
+++ src/com/swisscom/atclient/ATresponder.java
@@ -548,7 +548,7 @@ public class ATresponder extends Thread {
 						} else if (rx.toUpperCase().startsWith("+CNUM: ")) {
 							// <<< RX +CNUM: ,"+41797373717",145
 
-							msisdn = Arrays.asList(rx.split(",")).get(2);
+							msisdn = Arrays.asList(rx.split(",")).get(1);
 							Thread.currentThread().setName(Thread.currentThread().getName() + " " + msisdn);
 							
 						} else if (rx.toUpperCase().startsWith("+COPS: ")) {
